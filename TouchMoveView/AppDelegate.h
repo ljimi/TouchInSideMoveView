af59606d544d38ecf65b93d0fb9cd734ed9e392a
mxl <sunror@outlook.com>
2016-10-27 20:07:33 +0800

    Initial Commit

---
+++ TouchMoveView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchMoveView
+//
+//  Created by 孙春磊 on 2016/10/27.
+//  Copyright © 2016年 mxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchMoveView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchMoveView
+//
+//  Created by 孙春磊 on 2016/10/27.
+//  Copyright © 2016年 mxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
